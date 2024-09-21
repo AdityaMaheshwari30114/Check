7844dfe156d806567ba2e411ebc3058e0dca96e3
AdityaMaheshwari30114 <adityamaheshwari30114@gmail.com>
2024-09-21 12:37:09 +0530

    Initial

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  inputType: string="button";
+  inputType1: string="checkbox";
+  name: string="Palak";
+  selected: string='';
+
+  showWelcome(){
+    alert("Welcome")
+  }
+  showMessage(msg:string){
+    alert(msg);
+  }
+}
